5949b15a11f1c30afed772ed48a19c9a20189a16
btroost <59251634+btroost@users.noreply.github.com>
2025-02-13 11:39:43 +0100

    testcomponentenREST API toegevoegd

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import type { Schema } from "../amplify/data/resource";
 
 //Auth1
@@ -13,6 +13,45 @@ function App() {
   //Auth2
   const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleButtonClick = () => {
+    // Via de ref kun je toegang krijgen tot de waarde van het input veld
+    if (inputRef.current) {
+      alert(`De ingevoerde tekst is: ${inputRef.current.value}`);
+    }
+  };
+
+
+  const apiUrl = 'https://p0go49fgi5.execute-api.eu-west-3.amazonaws.com/Prod/hello';
+    
+
+  const fetchData = async () => {
+    alert('De knop "Get REST API" is ingedrukt');
+    try {
+      alert('checkpoint');
+      const response = await fetch(apiUrl);
+      alert('checkpoint2');
+      //alert('De knop "Get REST API" is ingedrukt. response is: ${response}');
+      alert(`De response is: ${response}`);
+     if (!response.ok) {
+        throw new Error('Netwerkresponse was niet ok');
+      }
+ 
+      const result = await response.json();
+      alert(`De ingevoerde tekst is: ${result}`);
+   //     const data: User[] = await response.json();
+ //     setUsers(data);
+    } catch (err) {
+      alert(`Er is een fout opgetreden ${err}` );
+      throw new Error('Er is een fout opgetreden: ');
+  // }
+    }
+  }
+
+
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -48,6 +87,14 @@ function App() {
         </a>
       </div>
       <button onClick={signOut}>Sign out</button>
+      <button onClick={fetchData}>Get REST API</button>
+      <input type="text" ref={inputRef} placeholder="Voer----- iets in" />
+      <button onClick={handleButtonClick}>Toon Ingevoerde Tekst</button>
+
+      <label>Invoer</label>
+
+      <div id="result">result</div>
+
     </main>
   );
 }
